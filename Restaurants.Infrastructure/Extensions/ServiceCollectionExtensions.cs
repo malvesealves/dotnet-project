a9cfb7faf054069047c125fbc7a6b4d2a7e7aa4e
Mateus Alves <m.alvesealves@gmail.com>
2025-01-20 12:03:58 -0300

    Partial dbContext

---
+++ Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddInterfaces(this IServiceCollection services, IConfiguration configuration)
+        {
+            string? connString = configuration.GetConnectionString("RestaurantsDb");
+            services.AddDbContext<RestaurantsDbContext>(options => options.UseSqlServer(connString));
+        }
+    }
+}
---
+++ Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Restaurants.Infrastructure.Persistence
+{
+    internal class RestaurantsDbContext(DbContextOptions dbContextOptions) : DbContext(dbContextOptions)
+    {
+        internal DbSet<Address> Addresses { get; set; }
+        internal DbSet<Customer> Customers { get; set; }
+        internal DbSet<Dish> Dishes { get; set; }
+        internal DbSet<Restaurant> Restaurants { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Restaurant>().OwnsOne(r => r.Address);
+
+            modelBuilder.Entity<Restaurant>().HasMany(r => r.Dishes).WithOne().HasForeignKey(d => d.RestaurantId);
+        }
+    }
+}
